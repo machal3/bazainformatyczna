7120c9ddefb32a9e99bb8ab0bde47ad8311e2b9d
machal3 <nowackimichalartur@gmail.com>
2024-10-01 19:55:33 +0200

    Dalej walę się z zadaniem zera

--- Algo24/DPK/zeros.cpp
+++ Algo24/DPK/zeros.cpp
@@ -10,121 +10,124 @@ struct wierzcholek
 };
 
 
-int tab[200000][200000];
-wierzcholek odp[200000][200000];
+int tab[2000000];
+wierzcholek odp[2000000];
 int n , m;
 
 int mniejsza (int a , int b)
 {
-	if (a > b)
-	{
-		return b;
-	}
-
-	return a;
+	return min (a , b);
 }
 
 int liczenie (int a , int b)
 {
-	int tym = mniejsza (a , b);
-	return tym;
+	return mniejsza (a , b);
 }
 
 int ilesiemiesci (int a , int b)
 {
 	int tym = 0;
-
-	while (true)
+	while (a % b == 0 && a > 0)
 	{
 		tym++;
 		a /= b;
-
-		if (a % b != 0)
-		{
-			return tym;
-		}
-
 	}
+	return tym;
 }
 
 int main ()
 {
 	cin >> n >> m;
 
-	for (int i = 1; i < n + 1; i++)
+	for (int i = 1; i <= n; i++)
 	{
-		for (int j = 1; j < m + 1; j++)
+		for (int j = 1; j <= m; j++)
 		{
-			cin >> tab[j][i];
+			cin >> tab[(i - 1) * m + (j - 1)];
 		}
 	}
 
-	for (int i = 2; i < m + 1; i++)
+	odp[0].min_2_dwa = ilesiemiesci (tab[0] , 2);
+	odp[0].min_5_dwa = ilesiemiesci (tab[0] , 2);
+	odp[0].min_2_piec = ilesiemiesci (tab[0] , 5);
+	odp[0].min_5_piec = ilesiemiesci (tab[0] , 5);
+
+	for (int i = 1; i < m; i++)
 	{
-		odp[i][0].min_2_dwa = INT_MAX;
-		odp[i][0].min_5_dwa = INT_MAX;
-		odp[i][0].min_2_piec = INT_MAX;
-		odp[i][0].min_5_piec = INT_MAX;
+		odp[i].min_2_dwa = odp[i - 1].min_2_dwa + ilesiemiesci (tab[i] , 2);
+		odp[i].min_5_dwa = odp[i - 1].min_5_dwa + ilesiemiesci (tab[i] , 2);
+		odp[i].min_2_piec = odp[i - 1].min_2_piec + ilesiemiesci (tab[i] , 5);
+		odp[i].min_5_piec = odp[i - 1].min_5_piec + ilesiemiesci (tab[i] , 5);
 	}
-	for (int i = 2; i < n + 1; i++)
+	for (int i = 1; i < n; i++)
 	{
-		odp[0][1].min_2_dwa = INT_MAX;
-		odp[0][1].min_5_dwa = INT_MAX;
-		odp[0][1].min_2_piec = INT_MAX;
-		odp[0][1].min_5_piec = INT_MAX;
+		odp[i * m].min_2_dwa = odp[(i - 1) * m].min_2_dwa + ilesiemiesci (tab[i * m] , 2);
+		odp[i * m].min_5_dwa = odp[(i - 1) * m].min_5_dwa + ilesiemiesci (tab[i * m] , 2);
+		odp[i * m].min_2_piec = odp[(i - 1) * m].min_2_piec + ilesiemiesci (tab[i * m] , 5);
+		odp[i * m].min_5_piec = odp[(i - 1) * m].min_5_piec + ilesiemiesci (tab[i * m] , 5);
 	}
 
-	for (int j = 1; j < n + 1; j++)
+	for (int i = 1; i < n; i++)
 	{
-		for (int i = 1; i < m + 1; i++)
+		for (int j = 1; j < m; j++)
 		{
-			if (odp[i - 1][j].min_2_dwa > odp[i][j - 1].min_2_dwa)
+			int indeks = i * m + j;
+			int lewy = i * m + (j - 1);
+			int gora = (i - 1) * m + j;
+
+
+			if (odp[lewy].min_2_dwa > odp[gora].min_2_dwa)
 			{
-				odp[i][j].min_2_dwa = odp[i][j - 1].min_2_dwa;
-				odp[i][j].min_2_piec = odp[i][j - 1].min_2_piec;
+				odp[indeks].min_2_dwa = odp[gora].min_2_dwa;
+				odp[indeks].min_2_piec = odp[gora].min_2_piec;
 			}
 
 			else
 			{
-				odp[i][j].min_2_dwa = odp[i - 1][j].min_2_dwa;
-				odp[i][j].min_2_piec = odp[i - 1][j].min_2_piec;
+				odp[indeks].min_2_dwa = odp[lewy].min_2_dwa;
+				odp[indeks].min_2_piec = odp[lewy].min_2_piec;
 			}
 
-			if (odp[i - 1][j].min_5_piec > odp[i][j - 1].min_5_piec)
+			if (odp[lewy].min_5_piec > odp[gora].min_5_piec)
 			{
-				odp[i][j].min_5_piec = odp[i][j - 1].min_5_piec;
-				odp[i][j].min_5_dwa = odp[i][j - 1].min_5_dwa;
+				odp[indeks].min_5_piec = odp[gora].min_5_piec;
+				odp[indeks].min_5_dwa = odp[gora].min_5_dwa;
 			}
 
 			else
 			{
 
-				odp[i][j].min_5_dwa = odp[i - 1][j].min_5_dwa;
-				odp[i][j].min_5_piec = odp[i - 1][j].min_5_piec;
+				odp[indeks].min_5_dwa = odp[lewy].min_5_dwa;
+				odp[indeks].min_5_piec = odp[lewy].min_5_piec;
 			}
-			if (tab[i][j] == 0)
+
+			if (tab[indeks] == 0)
 			{
 				cout << 0;
-				exit (0);
+				return 0;
 			}
-			else
-			{
-				if (tab[i][j] % 5 == 0)
-				{
-					odp[i][j].min_2_piec += ilesiemiesci (tab[i][j] , 5);
-					odp[i][j].min_5_piec += ilesiemiesci (tab[i][j] , 5);
-				}
 
-				if (tab[i][j] % 2 == 0)
-				{
-					odp[i][j].min_2_dwa += ilesiemiesci (tab[i][j] , 2);
-					odp[i][j].min_5_dwa += ilesiemiesci (tab[i][j] , 2);
-				}
+			if (tab[indeks] % 5 == 0)
+			{
+				odp[indeks].min_2_piec += ilesiemiesci (tab[indeks] , 5);
+				odp[indeks].min_5_piec += ilesiemiesci (tab[indeks] , 5);
 			}
+
+			if (tab[indeks] % 2 == 0)
+			{
+				odp[indeks].min_2_dwa += ilesiemiesci (tab[indeks] , 2);
+				odp[indeks].min_5_dwa += ilesiemiesci (tab[indeks] , 2);
+			}
+
+
 
 		}
 	}
 
-	cout << mniejsza (liczenie (odp[m][n].min_2_dwa , odp[m][n].min_2_piec) , liczenie (odp[m][n].min_5_dwa , odp[m][n].min_5_piec));
+	int wynik = mniejsza (liczenie (odp[(n - 1) * m + (m - 1)].min_2_dwa , odp[(n - 1) * m + (m - 1)].min_2_piec) , liczenie (odp[(n - 1) * m + (m - 1)].min_5_dwa , odp[(n - 1) * m + (m - 1)].min_5_piec)
+	);
 
+	cout << wynik;
+
+	return 0;
 }
