52a5958c982922f1b2d464097648cbc07b32d39b
Michał Nowacki <nowackimichalartur@gmail.com>
2024-11-18 16:20:10 +0100

    Delete Lekcje directory

--- Lekcje/Wczytaj-policz-wypisz/czek.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-main ()
-{
-    int w1 , w2 , k1 , k2;
-    cin >> w1 >> w2 >> k1 >> k2;
-    cout << (w1 * w2) + (k1 * k2);
-}
--- Lekcje/Wczytaj-policz-wypisz/sum2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-main ()
-{
-    int N;
-    cin >> N;
-    cout << (((N - 1) / 2) + 1);
-}
